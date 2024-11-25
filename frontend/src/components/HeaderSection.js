8958c236b2ead1f0f449734ae0bb37595614fbef
Silvana <silvana.gurlekian@davinci.edu.ar>
2024-11-25 11:46:35 -0300

    Actualizar header de acuerdo a la seccion que esta el usuario

--- frontend/src/components/HeaderSection.js
+++ frontend/src/components/HeaderSection.js
@@ -1,25 +1,36 @@
 import React from "react";
-import { useNavigate } from "react-router-dom"; // Importar useNavigate
+import { useNavigate, useLocation } from "react-router-dom"; 
 import "./HeaderSection.css";
 
 const HeaderSection = () => {
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
+  const location = useLocation(); 
 
+  // Función para ir a la pantalla anterior
   const handleBackClick = () => {
-    navigate("/home");
+    navigate(-1);  // Navegar a la página anterior en el historial
+  };
+
+  // Función para determinar el texto del encabezado según la ruta actual
+  const getHeaderText = () => {
+    if (location.pathname === "/profile") {
+      return "Mi cuenta"; 
+    } else if (location.pathname.startsWith("/detalle")) {
+      return "Detalle";
+    }
+    return ""; 
   };
 
   return (
     <div className="header-section">
       <header className="header">
         <div className="section" onClick={handleBackClick}>
-          {" "}
           <img
             className="white-icon"
             src="../images/icons/arrow-left.svg"
             alt="volver"
           />
-          <div className="text-wrapper">Mi cuenta</div>
+          <div className="text-wrapper">{getHeaderText()}</div>
         </div>
 
         <img src="../images/header-icon.svg" alt="icono" />
