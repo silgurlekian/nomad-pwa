f67a62846497b5ee11aab68c7f840a621b0eb498
Silvana <silvana.gurlekian@davinci.edu.ar>
2024-12-10 20:55:35 -0300

    cambia url de la pwa

--- frontend/public/service-worker.js
+++ frontend/public/service-worker.js
@@ -1,50 +1,49 @@
-// Nombre del caché
-const CACHE_NAME = 'nomad-cache-v1.1';
-
-// Archivos a almacenar en caché
+const CACHE_NAME = 'nomad-pwa-cache-v1';
 const urlsToCache = [
-  'https://pwa.nomad.com.ar/',
-  'https://pwa.nomad.com.ar/index.html',
-  'https://pwa.nomad.com.ar/styles.css',
-  'https://pwa.nomad.com.ar/app.js',
-  'https://pwa.nomad.com.ar/assets/logo.png',
+  '/pwa/',
+  '/pwa/icons/192x192.png',
+  '/pwa/icons/512x512.png'
 ];
 
 // Instalación del Service Worker
-self.addEventListener('install', (event) => {
-  console.log('Instalando el Service Worker...');
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('Archivos en caché correctamente');
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Caché creado y archivos añadidos.');
+        return cache.addAll(urlsToCache)
+          .catch(error => {
+            console.error('Error al añadir archivos a la caché:', error);
+          });
+      })
   );
 });
 
 // Activación del Service Worker
-self.addEventListener('activate', (event) => {
-  console.log('Activando el Service Worker...');
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then((cacheNames) =>
-      Promise.all(
-        cacheNames.map((cache) => {
-          // Elimina cachés antiguas que no coinciden con el CACHE_NAME actual
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
           if (cache !== CACHE_NAME) {
             console.log('Eliminando caché antigua:', cache);
             return caches.delete(cache);
           }
         })
-      )
-    )
+      );
+    })
   );
 });
 
-// Intercepción de solicitudes de red
-self.addEventListener('fetch', (event) => {
+// Interceptar solicitudes de red
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      // Devuelve la respuesta desde la caché si existe, o hace la solicitud a la red
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request)
+          .catch(error => {
+            console.error('Error al recuperar la solicitud:', error);
+          });
+      })
   );
 });
