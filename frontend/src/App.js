9d90e9243153a5361d7295fc4a0bf13c854a1c3b
Silvana <silvana.gurlekian@davinci.edu.ar>
2024-12-14 18:37:02 -0300

    forzar pantalla completa en pwa

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,10 +25,26 @@ function App() {
   const [showSplash, setShowSplash] = useState(true);
   const location = useLocation();
 
+  // Función para activar el modo pantalla completa
+  const enterFullScreen = () => {
+    if (document.documentElement.requestFullscreen) {
+      document.documentElement.requestFullscreen();
+    } else if (document.documentElement.mozRequestFullScreen) { // Firefox
+      document.documentElement.mozRequestFullScreen();
+    } else if (document.documentElement.webkitRequestFullscreen) { // Chrome, Safari, Opera
+      document.documentElement.webkitRequestFullscreen();
+    } else if (document.documentElement.msRequestFullscreen) { // IE/Edge
+      document.documentElement.msRequestFullscreen();
+    }
+  };
+
   useEffect(() => {
     const isRootPath =
       location.pathname === "/" || location.pathname === "/pwa/";
+
     if (isRootPath) {
+      enterFullScreen(); // Forzar pantalla completa cuando estamos en la ruta raíz
+
       const timer = setTimeout(() => {
         setShowSplash(false);
       }, 3000);
@@ -52,15 +68,12 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/reset-password" element={<RequestResetPassword />} />
           <Route path="/reset-password/:token" element={<ResetPassword />} />
-
           <Route path="/profile" element={<Profile />} />
           <Route path="/home" element={<Home />} />
           <Route path="/spaces" element={<SpacesList />} />
-
           <Route path="/spaces/:id" element={<SpaceDetail />} />
           <Route path="/reservation" element={<Reservation />} />
           <Route path="/reservation-success" element={<SuccessPage />} />
-
           <Route path="/favorites" element={<Favorites />} />
         </Routes>
       )}
